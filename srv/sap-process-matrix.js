126654efe86ef3dc333e13f2c3673bfb56cc8778
gauravb.shami@ltimindtree.com <gauravb.shami@ltimindtree.com>
2024-11-27 06:27:19 +0000

    upload seq logic removed

--- srv/sap-process-matrix.js
+++ srv/sap-process-matrix.js
@@ -109,87 +109,81 @@ module.exports = async function () {
      * Handler method called before creating data entry
      * for entity ProcessDocMedia.
      */
-    this.before('CREATE', ProcessDocMedia, async (req) => {
-        // console.log(req);
-        console.log("In before CREATE");
-        // Create Constructor for SequenceHelper 
-        // Pass the sequence name and db
-        const SeqReq = new SequenceHelper({
-            sequence: "MEDIA_ID",
-            db: db,
-        });
-        //Call method getNextNumber() to fetch the next sequence number 
-        let seq_no = await SeqReq.getNextNumber();
-        // Assign the sequence number to id element
-        sReqId = req.data.mediaId = seq_no;
-        //Assign the url by appending the id
-        // req.data.url = `/MediaLibSrv/ProcessDocMedia(${req.data.mediaId})/content`;
-    });
+    // this.before('CREATE', ProcessDocMedia, async (req) => {
+    //     // console.log(req);
+    //     console.log("In before CREATE");
+    //     // Create Constructor for SequenceHelper 
+    //     // Pass the sequence name and db
+    //     const SeqReq = new SequenceHelper({
+    //         sequence: "MEDIA_ID",
+    //         db: db,
+    //     });
+    //     //Call method getNextNumber() to fetch the next sequence number 
+    //     let seq_no = await SeqReq.getNextNumber();
+    //     // Assign the sequence number to id element
+    //     sReqId = req.data.mediaId = seq_no;
+    //     //Assign the url by appending the id
+    //     // req.data.url = `/MediaLibSrv/ProcessDocMedia(${req.data.mediaId})/content`;
+    // });
 
-    this.after('CREATE', ProcessDocMedia, async (req) => {
-        // console.log(req);
-        console.log("In After CREATE");
-        // Create Constructor for SequenceHelper 
-        // Pass the sequence name and db
-        UPDATE (SAPProcessTree, req.data.processId) .with ({
-            processflow: sReqId     
-          });
-    });
+    // this.after('CREATE', ProcessDocMedia, async (req) => {
+    //     // console.log(req);
+    //     console.log("In After CREATE");
+    //     // Create Constructor for SequenceHelper 
+    //     // Pass the sequence name and db
+    //     UPDATE (SAPProcessTree, req.data.processId) .with ({
+    //         processflow: sReqId     
+    //       });
+    // });
 
     /**
      * Handler method called on reading data entry
      * for entity ProcessDocMedia.
      **/
-    this.on("READ", ProcessDocMedia, async (req, next) => {
-        if (!req.data.mediaId) {
-            return next();
-        }
-        //Fetch the url from where the req is triggered
-        const url = req._.req.path;
-        //If the request url contains keyword "content"
-        // then read the media content
-        if (url.includes("content")) {
-            const iMediaId = req.data.mediaId;
-            // var tx = cds.transaction(req);
-            // // Fetch the media obj from database
-            // var mediaObj = await tx.run(
-            //     SELECT.one.from("Media.db.ProcessDocMedia", ["content", "mediaType"]).where(
-            //         "id =",
-            //         id
-            //     )
-            // );
+    // this.on("READ", ProcessDocMedia, async (req, next) => {
+    //     console.log("in Media Read");
+    //     if (!req.data.mediaId) {
+    //         return next();
+    //     }
+    //     //Fetch the url from where the req is triggered
+    //     const url = req._.req.path;
+    //     //If the request url contains keyword "content"
+    //     // then read the media content
+    //     if (url.includes("content")) {
+    //         const iMediaId = req.data.mediaId;
+            
 
-            var mediaObj = await SELECT.one.from (ProcessDocMedia) .where ({ id: iMediaId });
-            // .columns ( oProcessDocMediaRow => { oProcessDocMediaRow.content, oProcessDocMediaRow.mediaType })       
+    //         var mediaObj = await SELECT.one.from (ProcessDocMedia) .where ({ id: iMediaId });
+    //         // .columns ( oProcessDocMediaRow => { oProcessDocMediaRow.content, oProcessDocMediaRow.mediaType })       
 
 
-            if (mediaObj.length <= 0) {
-                req.reject(404, "Media not found for the ID");
-                return;
-            }
-            var decodedMedia = "";
-            decodedMedia = new Buffer.from(
-                mediaObj.content.toString().split(";base64,").pop(),
-                "base64"
-            );
-            return _formatResult(decodedMedia, mediaObj.mediaType,  mediaObj.filename, mediaObj.processId);
-        } else return next();
-    });
+    //         if (mediaObj.length <= 0) {
+    //             req.reject(404, "Media not found for the ID");
+    //             return;
+    //         }
+    //         var decodedMedia = "";
+    //         decodedMedia = new Buffer.from(
+    //             mediaObj.content.toString().split(";base64,").pop(),
+    //             "base64"
+    //         );
+    //         return _formatResult(decodedMedia, mediaObj.mediaType,  mediaObj.filename, mediaObj.processId);
+    //     } else return next();
+    // });
 
-    function _formatResult(decodedMedia, mediaType, filename, processId) {
-        const readable = new Readable();
-        // const result = new Array();
-        readable.push(decodedMedia);
-        readable.push(null);
-        return {
-            value: readable,
-            '*@odata.mediaContentType': mediaType,
-            filename: filename,            
-            processId: processId
-        }
+    // function _formatResult(decodedMedia, mediaType, filename, processId) {
+    //     const readable = new Readable();
+    //     // const result = new Array();
+    //     readable.push(decodedMedia);
+    //     readable.push(null);
+    //     return {
+    //         value: readable,
+    //         '*@odata.mediaContentType': mediaType,
+    //         filename: filename,            
+    //         processId: processId
+    //     }
 
     
-    }
+    // }
 
 }
 
