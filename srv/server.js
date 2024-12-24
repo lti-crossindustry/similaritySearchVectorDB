69d795c7387ebacffd93cd0eee5beda96837d147
gauravb.shami@ltimindtree.com <gauravb.shami@ltimindtree.com>
2024-12-24 10:30:25 +0000

    timeout increase

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,9 @@
 const cds = require("@sap/cds");
 const cov2ap = require("@cap-js-community/odata-v2-adapter");
 cds.on("bootstrap", (app) => app.use(cov2ap()));
+
+cds.once('listening', ({ server }) => {
+    server.keepAliveTimeout = 5 * 60 * 1000 // > 5 mins
+  });
+
 module.exports = cds.server;
